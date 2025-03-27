ca1fb04e82b1ae679e00c33e0a3358de39e13b9e
Anlor <chenxiex@outlook.com>
2025-03-27 16:29:04 +0800

    修复wpscloudsvr.real无法更新

--- wpscloudsvr/install.sh
+++ wpscloudsvr/install.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 dir=$(dirname $0)
-if [ ! -e /opt/kingsoft/wps-office/office6/wpscloudsvr.real ]; then
-    mv /opt/kingsoft/wps-office/office6/wpscloudsvr /opt/kingsoft/wps-office/office6/wpscloudsvr.real 
+
+# 检查wpscloudsvr是否为ELF文件
+if file /opt/kingsoft/wps-office/office6/wpscloudsvr | grep -q "ELF"; then
+    mv /opt/kingsoft/wps-office/office6/wpscloudsvr /opt/kingsoft/wps-office/office6/wpscloudsvr.real
+else
+    echo "wpscloudsvr is not ELF file, skip"
 fi
 cp ${dir}/wpscloudsvr /opt/kingsoft/wps-office/office6/
-cp ${dir}/monitor.sh /opt/kingsoft/wps-office/office6/ 
+cp ${dir}/monitor.sh /opt/kingsoft/wps-office/office6/
 cp ${dir}/wpscs_monitor.service /etc/systemd/user/
 systemctl daemon-reload
